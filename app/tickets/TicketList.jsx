edb029a678c59484f2c130123e0fa24a47cbe7b5
justintaekim <justinkim607@berkeley.edu>
2024-06-20 18:04:59 -0700

    dynamic segments and fetching data

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+
+// responsible for fetching
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 10, // use 0 to opt out of using cache
+    },
+  });
+
+  return res.json();
+}
+
+// server component - make it async and fetch directly
+export default async function TicketList() {
+  const tickets = await getTickets();
+
+  //
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets, yay!</p>
+      )}
+    </>
+  );
+}
